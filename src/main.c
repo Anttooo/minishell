2c21f508ffcf5efc75610a164b9f66b3dd82e564
Onni Anttoora <oanttoor@c2r4p6.hive.fi>
2023-03-28 17:29:47 +0300

    header added to main.c

--- src/main.c
+++ src/main.c
@@ -1,29 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oanttoor <oanttoor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 17:26:35 by oanttoor          #+#    #+#             */
+/*   Updated: 2023/03/28 17:29:29 by oanttoor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdio.h>
 
 int main(void)
 {
     char *input;
 
     while (42) {
-        input = readline("Supershell >> ");
+        input = readline("Supershell> ");
         if (!input) 
-		{
             printf("\n");
-        }
-		else 
-		{
-			printf("You entered: %s\n", input);
-		}
-
         if (strcmp(input, "quit") == 0) 
 		{
             free(input);
             break;
         }
-		
         free(input);
     }
     return (0);
