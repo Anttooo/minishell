81a5e3aefdb0e95e86a442947d08c870bddbb02c
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-03-29 09:12:23 +0300

    did changes we talked about to function name in input.c

--- include/input.h
+++ include/input.h
@@ -6,7 +6,7 @@
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 08:56:37 by joonasmykka       #+#    #+#             */
-/*   Updated: 2023/03/29 09:05:02 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/03/29 09:10:30 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 17:26:35 by oanttoor          #+#    #+#             */
-/*   Updated: 2023/03/29 08:57:44 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/03/29 09:10:24 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int main(void)
         if (!input) 
             printf("\n");
 		else
-			input_handle(input);
+		{
+			if (handle_input(input) < 0)
+		}
         free(input);
     }
     return (0);
