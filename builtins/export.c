b2eb2f25f02c87ee4c9ecc768d81879bed98f66b
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-04-02 07:31:21 +0300

    removed executables logic and added only .c files for builtins

--- builtins/export.c
+++ builtins/export.c
@@ -1,30 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 10:38:38 by joonasmykka       #+#    #+#             */
-/*   Updated: 2023/03/31 10:26:38 by joonasmykka      ###   ########.fr       */
+/*   Updated: 2023/04/02 07:30:31 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "echo.h"
+#include "../include/minishell.h"
 
-// TODO:
-// Input will be options(maybe) and arguments to print
-// needs to scan for $wildcard character to possibly print from variable
-// this is probalby easist to be done if args are printed one by one and if stumble upon wildcard
-// enter a loop to print that, and then return to main loop printing args one by one.
-void	echo(int argc, char	**argv)
+int	export(int argc, char **argv, char **envp)
 {
+	printf("Using builtin function. \n");
 	
-}
+	int i;
 
-int	main(int argc, char **argv)
-{
-	if (argc > 1)
-		echo(argc, argv);
-	return (0);
+	i = 0;
+	while (envp[i]) 
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
+	if (argc == 2)
+	{
+		printf("%s\n", argv[1]);
+		return (1);
+	}
+	return (-1);
 }
--- builtins/pwd.c
+++ builtins/pwd.c
@@ -1,18 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.h                                              :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joonasmykkanen <joonasmykkanen@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/29 20:22:05 by joonasmykka       #+#    #+#             */
-/*   Updated: 2023/03/31 08:26:18 by joonasmykka      ###   ########.fr       */
+/*   Created: 2023/03/30 10:38:38 by joonasmykka       #+#    #+#             */
+/*   Updated: 2023/04/02 07:30:38 by joonasmykka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PWD_H
-# define PWD_H
+#include "../include/minishell.h"
 
-# include <stdio.h>
-
-#endif
+int	pwd(int argc, char **argv)
+{
+	printf("Using builtin function. \n");
+	if (argc == 2)
+	{
+		printf("%s\n", argv[1]);
+		return (1);
+	}
+	return (-1);
+}
